e91a0e3406c2b422ddce486d902bf9655340f5a4
Chatresh Ramasai Gudi <chatreshrsg0202@gmail.com>
2026-04-17 11:03:14 +0530

    Phase 4: Serialize commit and write to object store as OBJ_COMMIT

--- commit.c
+++ commit.c
@@ -215,6 +215,15 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     commit.timestamp = (uint64_t)time(NULL);
     snprintf(commit.message, sizeof(commit.message), "%s", message);
 
+    // Step 4: Serialize to text and write to the object store
+    void *data;
+    size_t len;
+    if (commit_serialize(&commit, &data, &len) < 0) return -1;
+
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) < 0) {
+        free(data); return -1;
+    }
+    free(data);
 
     // TODO: read parent, fill commit struct, write object, update HEAD
     (void)message; (void)commit_id_out;
